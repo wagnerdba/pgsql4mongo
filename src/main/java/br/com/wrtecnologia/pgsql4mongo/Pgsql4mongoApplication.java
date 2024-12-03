0885fff63acb110e5f10012e5bebcfa659492f0b
Wagner Pires <wagnerdba@gmail.com>
2024-12-02 21:52:10 -0300

    initial commit

---
+++ src/main/java/br/com/wrtecnologia/pgsql4mongo/Pgsql4mongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.wrtecnologia.pgsql4mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pgsql4mongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pgsql4mongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/wrtecnologia/pgsql4mongo/Pgsql4mongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wrtecnologia.pgsql4mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pgsql4mongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
